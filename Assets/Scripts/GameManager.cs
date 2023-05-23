004d687ca5e65dbc4e98da3984993d5272de6aa1
A. Roig <antiaroig@gmail.com>
2023-05-23 21:02:09 +0200

    Código funcional

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class GameManager : NetworkBehaviour
+{
+
+    [SerializeField] private int maxPlayersPerTeam = 2;
+
+    public static GameManager Instance;
+    public List<Material> materials; // 0 -> blanco | 1 a 3 -> Left | 4 a 6 -> Right
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else if (Instance != this) Destroy(gameObject);
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        {
+            StartButtons();
+        }
+        else
+        {
+            StatusLabels();
+            SubmitNewPositionButton();
+        }
+
+        GUILayout.EndArea();
+    }
+
+    static void StartButtons()
+    {
+        if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+        if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+        if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+    }
+
+    static void StatusLabels()
+    {
+        var mode = NetworkManager.Singleton.IsHost ?
+            "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+
+        GUILayout.Label("Transport: " + NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+        GUILayout.Label("Mode: " + mode);
+        
+    }
+
+    private static void SubmitNewPositionButton()
+    {
+        if (GUILayout.Button("Mover ao inicio"))
+        {
+            if (NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient)
+            {
+                foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
+                {
+                    NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<Player>().MoveToStartServerRpc();
+                }
+
+            }
+            else
+            {
+                var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
+                playerObject.GetComponent<Player>().MoveToStartServerRpc();
+            }
+        }
+    }
+
+
+    public void CheckMovementActivation()
+    {
+        // Recorremos todos os clientes para comprobar cantos hai de cada equipo
+        List<Player> leftPlayers = new List<Player>();
+        List<Player> rightPlayers = new List<Player>();
+        List<Player> whitePlayers = new List<Player>();
+        foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
+        {
+            Player player = NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<Player>();
+            if (player.ColorIndex.Value > 0 && player.ColorIndex.Value <= 3)
+            {
+                leftPlayers.Add(player);
+            }
+            else if (player.ColorIndex.Value > 3 && player.ColorIndex.Value <= 6)
+            {
+                rightPlayers.Add(player);
+            }
+            else if (player.ColorIndex.Value == 0)
+            {
+                whitePlayers.Add(player);
+            }
+        }
+
+
+        // Se hai dous de calquera dos equipos, desactivamos o movemento do contrario e, 
+        // por se acaso non estivese activo xa, activamos o propio
+        if (leftPlayers.Count == maxPlayersPerTeam || rightPlayers.Count == maxPlayersPerTeam)
+        {
+            foreach (Player player in leftPlayers)
+                player.EnableOrDisableMovementClientRpc(leftPlayers.Count == maxPlayersPerTeam);
+
+            foreach (Player player in rightPlayers)
+                player.EnableOrDisableMovementClientRpc(rightPlayers.Count == maxPlayersPerTeam);
+            
+            foreach (Player player in whitePlayers)
+                player.EnableOrDisableMovementClientRpc(false);
+        }
+        else
+        // No caso contrario (non hai 2 xogadores en ningún equipo) unificamos as listas
+        // e activamos o movemento de todos
+        {
+            leftPlayers.AddRange(rightPlayers);
+            leftPlayers.AddRange(whitePlayers);
+            foreach (Player player in leftPlayers)
+            {
+                player.EnableOrDisableMovementClientRpc(true);
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,114 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+
+    private float baseSpeed = 5f;
+    private MeshRenderer mr;
+
+    public NetworkVariable<int> ColorIndex = new NetworkVariable<int>();
+    public NetworkVariable<bool> CanMove = new NetworkVariable<bool>();
+
+    public override void OnNetworkSpawn()
+    {
+        ColorIndex.OnValueChanged += OnColorChanged;
+        CanMove.Value = true;
+
+        mr = GetComponent<MeshRenderer>();
+
+        if (IsOwner) InitializeServerRpc();
+        else mr.material = GameManager.Instance.materials[ColorIndex.Value];
+
+        GameManager.Instance.CheckMovementActivation();
+    }
+    public override void OnNetworkDespawn()
+    {
+        ColorIndex.OnValueChanged -= OnColorChanged;
+    }
+
+    void Update()
+    {
+        if (IsOwner && CanMove.Value)
+        {
+            if (Input.GetKey(KeyCode.W)) MoveRequestServerRpc(Vector3.forward);
+            if (Input.GetKey(KeyCode.S)) MoveRequestServerRpc(Vector3.back);
+            if (Input.GetKey(KeyCode.A)) MoveRequestServerRpc(Vector3.left);
+            if (Input.GetKey(KeyCode.D)) MoveRequestServerRpc(Vector3.right);
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                MoveToStartServerRpc();
+            }
+        }
+    }
+
+    [ServerRpc]
+    private void MoveRequestServerRpc(Vector3 direction)
+    {
+        transform.position += direction * baseSpeed * Time.deltaTime;
+    }
+
+
+    [ServerRpc]
+    void InitializeServerRpc()
+    {
+        ColorIndex.Value = 0;  // Así sempre se executará o método "OnColorChanged" no inicio
+        transform.position = new Vector3(Random.Range(-2.5f, 2.5f), 1.2f, Random.Range(-3f, 3f));
+    }
+
+    [ServerRpc]
+    internal void MoveToStartServerRpc()
+    {
+        bool toLeft = Random.Range(0, 2) == 0;
+        float x = toLeft ? Random.Range(-9.5f, -3f) : Random.Range(3f, 9.5f);
+        transform.position = new Vector3(x, 1.2f, Random.Range(-3f, 3f)); ;
+    }
+
+    [ClientRpc]
+    internal void EnableOrDisableMovementClientRpc(bool isEnabled)
+    {
+        CanMove.Value = isEnabled;
+    }
+
+    public void OnColorChanged(int previous, int current)
+    {
+        mr.material = GameManager.Instance.materials[current];
+        GameManager.Instance.CheckMovementActivation();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Left"))
+            ColorIndex.Value = SetUnrepeatedRandomColor(1,3);
+        else if (other.CompareTag("Right"))
+            ColorIndex.Value = SetUnrepeatedRandomColor(4, 6);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ColorIndex.Value = 0;
+    }
+
+    private int SetUnrepeatedRandomColor(int min, int max)
+    {
+        bool isRepeated = false;
+        int randomIndexColor;
+
+        do
+        {
+            randomIndexColor = Random.Range(min, max + 1);
+            foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
+            {
+                isRepeated = false;
+                if (randomIndexColor == NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<Player>().ColorIndex.Value)
+                {
+                    isRepeated = true;
+                    break;
+                }
+            }
+        } while (isRepeated);
+
+        return randomIndexColor;
+    }
+
+}
